7c4776b199714c87da61bd4f6291052568e3484b
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2024-02-29 13:52:16 +0100

    feat: setup

--- lib/main.dart
+++ lib/main.dart
@@ -9,34 +9,15 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'PulsAid',
       theme: ThemeData(
-        useMaterial3: true,
-        // Define the default brightness and colors.
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.purple,
-          // ···
-          brightness: Brightness.light,
-        ),
-
-        // Define the default `TextTheme`. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-        textTheme: TextTheme(
-          displayLarge: const TextStyle(
-            fontSize: 72,
-            fontWeight: FontWeight.bold,
-          ),
-          // ···
-          titleLarge: GoogleFonts.oswald(
-            fontSize: 30,
-            fontStyle: FontStyle.italic,
-          ),
-          bodyMedium: GoogleFonts.merriweather(),
-          displaySmall: GoogleFonts.pacifico(),
-        ),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(
-        title: appName,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
       ),
     );
   }
