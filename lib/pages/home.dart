015289dd7f33f8dd4646b3869e369f7aa3ba0dd2
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2024-03-14 12:27:41 +0100

    Fix: home login check

---
+++ lib/pages/home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:theapp/app_localizations.dart';
+import 'package:theapp/colors.dart';
+import 'package:theapp/components/buttons/button_blue.dart';
+import 'package:theapp/main.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool loggedin = true; // replace with your actual condition
+
+  @override
+  void initState() {
+    super.initState();
+    if (false == GlobalVariables().loggedin) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.pushNamed(context, '/language');
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(
+              top: 50,
+              left: 20,
+              right: 20,
+            ),
+            child: Text(
+              AppLocalizations.of(context).translate('how_would_you_like_to_help'),
+              style: const TextStyle(
+                color: BrandColors.gray,
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
