d51107d97f60f9e2ac67215322282bc0177129e5
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2024-04-11 15:26:02 +0200

    Fix: add animation and text to loading screen

--- lib/pages/save_registration.dart
+++ lib/pages/save_registration.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:theapp/app_localizations.dart';
 import 'package:theapp/classes/registration_data.dart';
 import 'package:theapp/classes/apimanager.dart';
+import 'package:theapp/colors.dart';
+import 'package:theapp/components/animations/heart.dart';
 
 class SaveRegistrationPage extends StatelessWidget {
   const SaveRegistrationPage({super.key});
@@ -16,10 +19,33 @@ class SaveRegistrationPage extends StatelessWidget {
         Navigator.pushNamed(context, '/login');
       } 
     });
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
+return Scaffold(
+  backgroundColor: BrandColors.offWhiteDark,
+  body: Align(
+    alignment: Alignment.center,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center, // Voeg deze regel toe
+      children: <Widget>[
+        const Flexible(
+          flex: 1,
+          child: SizedBox(
+            height: 250,
+            child: HeartAnimation(),
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: Text(
+              AppLocalizations.of(context).translate('welcome_hero'),
+              style: const TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
+      ],
+    ),
+  ),
+);
   }
 }
