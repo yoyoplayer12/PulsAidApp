06035ae6f92ad2fda6cc044fc89072ca3ea5aca2
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2024-04-21 10:07:25 +0200

    Fix: nav bar in two languages

--- lib/components/navbar.dart
+++ lib/components/navbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:theapp/app_localizations.dart';
 import 'package:theapp/colors.dart';
 
 
@@ -17,6 +18,20 @@ class _CustomNavBarState extends State<CustomNavBar> {
     setState(() {
       _selectedIndex = index;
     });
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, '/');
+        break;
+      case 1:
+        Navigator.pushNamed(context, '/doNotDisturb');
+        break;
+      case 2:
+        Navigator.pushNamed(context, '/instructions');
+        break;
+      case 3:
+        Navigator.pushNamed(context, '/account');
+        break;
+    }
   }
 
   @override
@@ -40,11 +55,11 @@ class _CustomNavBarState extends State<CustomNavBar> {
           ),
           BottomNavigationBarItem(
             icon: _selectedIndex == 1 ? const Icon(Icons.alarm_on) : const Icon(Icons.alarm_on_outlined),
-            label: 'Time out',
+            label: AppLocalizations.of(context).translate('do_not_disturb'),
           ),
           BottomNavigationBarItem(
             icon: _selectedIndex == 2 ? const Icon(Icons.play_lesson) : const Icon(Icons.play_lesson_outlined),
-            label: 'Instructions',
+            label: AppLocalizations.of(context).translate('instructions'),
           ),
           BottomNavigationBarItem(
             icon: _selectedIndex == 3 ? const Icon(Icons.account_circle) : const Icon(Icons.account_circle_outlined),
