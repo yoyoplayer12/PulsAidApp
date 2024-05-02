128a1f12a54fee59b4060b280adb6e74394aec1f
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2024-05-02 12:34:59 +0200

    Feat: allow while using app popup android + ios

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,54 @@ import 'package:theapp/classes/route_generator.dart';
 import 'package:theapp/colors.dart';
 import 'package:provider/provider.dart';
 import 'package:theapp/classes/registration_data.dart';
+import 'package:geolocator/geolocator.dart';
 
+/// Determine the current position of the device.
+///
+/// When the location services are not enabled or permissions
+/// are denied the `Future` will return an error.
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the 
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale 
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+  
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately. 
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.');
+  } 
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
+}
 
 Future main() async {
   await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool? loggedin = prefs.getBool('loggedin');
-
   runApp(ChangeNotifierProvider(
       create: (context) => RegistrationData(),
       child: MyApp(loggedin: loggedin ?? false),
@@ -34,6 +74,12 @@ Future main() async {
   OneSignal.initialize(dotenv.env['ONESIGNAL_APP_ID']!);
   OneSignal.Notifications.requestPermission(true);
 
+  try {
+      Position position = await _determinePosition();
+      print('Current position: $position');
+    } catch (e) {
+      print('Error determining position: $e');
+    }
 }
 
 class MyApp extends StatefulWidget {
