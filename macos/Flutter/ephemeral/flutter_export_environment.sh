64f6cfb5fd7c36f660c36fd150a7a0d539439db6
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2024-05-28 20:54:09 +0200

    Fix: versioning

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Evelien Vanophalvens\OneDrive - Thomas More\lab3\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Evelien Vanophalvens\OneDrive - Thomas More\lab3\pulsaid"
+export "FLUTTER_ROOT=/Users/yorickdv/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/yorickdv/Desktop/School/3IMD/Pulsaid/application"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.2.0"
-export "FLUTTER_BUILD_NUMBER=0.2.0"
+export "FLUTTER_BUILD_NAME=0.3.0"
+export "FLUTTER_BUILD_NUMBER=0.3.0"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
