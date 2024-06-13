a71f97e602ae9a6470550b011ac134b1b9896853
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2024-06-13 14:40:40 +0200

    Feat: fix tussenscherm luisterend oor hulp

--- lib/pages/conversation_loader_ear.dart
+++ lib/pages/conversation_loader_ear.dart
@@ -4,7 +4,6 @@ import 'package:theapp/classes/apimanager.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-
 class ConversationLoaderEar extends StatefulWidget {
   final String platform;
 
@@ -16,41 +15,63 @@ class ConversationLoaderEar extends StatefulWidget {
 }
 
 class _ConversationLoaderEarState extends State<ConversationLoaderEar> {
-  //logincheck
+  bool _isAvailable = false;
+
   @override
   void initState() {
     super.initState();
   }
 
-  //main content
+  void _setAvailability(bool isAvailable) {
+    setState(() {
+      _isAvailable = isAvailable;
+    });
+
+    if (!isAvailable) {
+      Navigator.pop(context); // Go home if not available
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return PopScope(
+      canPop: false, // Prevent automatic popping of current route
+      child: Scaffold(
         appBar: AppBar(
-          title: Text(AppLocalizations.of(context)
-              .translate("initiating_conversation")),
-          leading: BackButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
+          automaticallyImplyLeading: false,
+          title: Text(_isAvailable
+              ? AppLocalizations.of(context).translate("initiating_conversation")
+              : AppLocalizations.of(context).translate("initiating_conversation")),
         ),
         body: Center(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 24.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(bottom: 40.0),
-                  child: Text(
-                    'You should receive a message on ${widget.platform} in a few minutes.',
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ],
+              children: _isAvailable
+                  ? [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 40.0),
+                        child: Text(
+                          'You should receive a message on ${widget.platform} in a few minutes.',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ]
+                  : [
+                      ElevatedButton(
+                        onPressed: () => _setAvailability(true),
+                        child: Text(AppLocalizations.of(context).translate("im_available")),
+                      ),
+                      ElevatedButton(
+                        onPressed: () => _setAvailability(false),
+                        child: Text(AppLocalizations.of(context).translate("im_not_available")),
+                      ),
+                    ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
